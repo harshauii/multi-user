692795fa718bd7488c7d2cdddaf65b9803e86325
harshauii <harshavardhanjw@gmail.com>
2025-05-20 16:40:29 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,9 +4,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyAKz4LhJcThTJ1ZowKYMV7rVlKwDhuXz3g",
+  authDomain: "rolebased-test-1.firebaseapp.com",
+  databaseURL: "https://rolebased-test-1-default-rtdb.firebaseio.com",
+  projectId: "rolebased-test-1",
+  storageBucket: "rolebased-test-1.firebasestorage.app",
+  messagingSenderId: "951301430114",
+  appId: "1:951301430114:web:17cae16a755c2cbc086a3b",
+  measurementId: "G-CFC2EF9MQF"
 };
 
 const app = initializeApp(firebaseConfig);
