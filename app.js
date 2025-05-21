444c9782d95e56dad75b490c4b391476749e9a6c
harshauii <harshavardhanjw@gmail.com>
2025-05-21 12:55:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,14 @@ import {
 
 // Replace with your config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyAKz4LhJcThTJ1ZowKYMV7rVlKwDhuXz3g",
+  authDomain: "rolebased-test-1.firebaseapp.com",
+  databaseURL: "https://rolebased-test-1-default-rtdb.firebaseio.com",
+  projectId: "rolebased-test-1",
+  storageBucket: "rolebased-test-1.firebasestorage.app",
+  messagingSenderId: "951301430114",
+  appId: "1:951301430114:web:17cae16a755c2cbc086a3b",
+  measurementId: "G-CFC2EF9MQF"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -85,3 +90,9 @@ onAuthStateChanged(auth, async (user) => {
     }
   }
 });
+// Logout Function
+window.logout = function () {
+  auth.signOut().then(() => {
+    window.location.href = "index.html";
+  });
+};
